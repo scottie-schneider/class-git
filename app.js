c215928003f491e9c14f9d7b585311e8fd1c7775
rcrutherford <rcrutherford@gmail.com>
2017-08-28 20:56:11 -0500

    Auto stash before merge of "master" and "origin/master"

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ let bodyParser = require('body-parser');
 let mongoose = require('mongoose');
 let app = express();
 
+//ron added this comment  - suk it 2
 
 // mongodb connection
 mongoose.connect("mongodb://localhost:27017/classGrader");
